5392500d7b8e5425899c62ce0444966f3e966912
Damien Dosimont <damien.dosimont@imag.fr>
2014-11-25 18:15:54 +0100

    thesis

---
+++ 2014/11/25/thesis.mk
@@ -0,0 +1,13 @@
+* Thesis TODO List
+
+** Général
+
+- [ ] Evoquer la différence entre notre technique d'agrégation et le clustering
+- [ ] Test
+
+** Chapitres
+
+*** Chapitre 6
+
+- [ ] Figures avec les agrégats interdits
+- [ ] Test
